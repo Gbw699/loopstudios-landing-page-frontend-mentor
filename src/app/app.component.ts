0c4f87c969f4a5aea43029f9846819c18361ae71
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-04-11 14:17:10 -0300

    Add Background component and integrate it into the main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { MobileMenuComponent } from './components/mobile-menu/mobile-menu.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MobileMenuComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MobileMenuComponent,
+    BackgroundComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
